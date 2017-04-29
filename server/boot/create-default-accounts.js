82ece988299c03a8a67fbd134517a773a4262e0e
Khanh Hoang <hoangchaukhanh93@gmail.com>
2017-04-30 00:07:27 +0700

    Add authentication rules for Account model

--- server/boot/create-default-accounts.js
+++ server/boot/create-default-accounts.js
@@ -1,56 +1,52 @@
 module.exports = app => {
   const { Account, Role, RoleMapping } = app.models
 
-  app.dataSources.noteSth.autoupdate('Account', (error) => {
+  // Find accounts in the system
+  Account.find({ where: { username: { inq: ['hckhanh', 'demo'] } } }, (error, users) => {
     if (error) throw error
 
-    // Find accounts in the system
-    Account.find({ where: { username: { inq: ['hckhanh', 'demo'] } } }, (error, users) => {
-      if (error) throw error
+    // If the default accounts are not existed, create them
+    if (users.length === 0) {
+      Account.create([
+        {
+          email: 'hoangchaukhanh93@gmail.com',
+          password: 'admin123',
+          username: 'hckhanh'
+        },
+        {
+          email: 'demo@example.com',
+          password: 'demo',
+          username: 'demo'
+        }
+      ], (error, users) => {
+        if (error) throw error
 
-      // If the default accounts are not existed, create them
-      if (users.length === 0) {
-        Account.create([
-          {
-            email: 'hoangchaukhanh93@gmail.com',
-            password: 'admin123',
-            username: 'hckhanh'
-          },
-          {
-            email: 'demo@example.com',
-            password: 'demo',
-            username: 'demo'
-          }
-        ], (error, users) => {
+        // Create "admin" role
+        Role.findOrCreate({ name: 'admin' }, (error, role) => {
           if (error) throw error
 
-          // Create "admin" role
-          Role.findOrCreate({ name: 'admin' }, (error, role) => {
+          // Create admin mapping to admin user
+          role.principals.create({
+            principalType: RoleMapping.USER,
+            principalId: users[0].id
+          }, (error, principal) => {
             if (error) throw error
-
-            // Create admin mapping to admin user
-            role.principals.create({
-              principalType: RoleMapping.USER,
-              principalId: users[0].id
-            }, (error, principal) => {
-              if (error) throw error
-            })
-          })
-
-          // Create "demo" role
-          Role.findOrCreate({ name: 'demo' }, (error, role) => {
-            if (error) throw error
-
-            // Create admin mapping to admin user
-            role.principals.create({
-              principalType: RoleMapping.USER,
-              principalId: users[1].id
-            }, (error, principal) => {
-              if (error) throw error
-            })
           })
         })
-      }
-    })
+
+        // Create "demo" role
+        Role.findOrCreate({ name: 'demo' }, (error, role) => {
+          if (error) throw error
+
+          // Create admin mapping to admin user
+          role.principals.create({
+            principalType: RoleMapping.USER,
+            principalId: users[1].id
+          }, (error, principal) => {
+            if (error) throw error
+          })
+        })
+      })
+    }
   })
 }

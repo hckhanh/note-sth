310c7d693da3557ef1e552e0c7f5e24dd5411ddc
Khanh Hoang <hoangchaukhanh93@gmail.com>
2017-05-08 23:59:54 +0700

    Add socketToken api for ably service

--- common/models/account.js
+++ common/models/account.js
@@ -1,5 +1,18 @@
-'use strict';
+'use strict'
+
+const Ably = require('ably')
+const ably = new Ably.Rest({ key: process.env.ABLY_API_KEY })
 
 module.exports = function(Account) {
-
-};
+  /**
+   * Get authentication token from socket service (realtime)
+   * @param {string} id Account id
+   * @param {Function(Error, object)} callback
+   */
+  Account.getSocketToken = function(id, callback) {
+    ably.auth.requestToken({ clientId: id }, function(error, socketToken) {
+      if (error) return callback(error)
+      callback(null, socketToken)
+    })
+  }
+}
